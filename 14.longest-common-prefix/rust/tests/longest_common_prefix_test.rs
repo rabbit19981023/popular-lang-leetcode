e78c1756daa4c7b936d3dcbf9828bf0b6596139d
rabbit19981023 <rabbit19981023@gmail.com>
2022-03-04 19:48:26 +0800

    fmt 14.longest-common-prefix lang:rust

--- 14.longest-common-prefix/rust/tests/longest_common_prefix_test.rs
+++ 14.longest-common-prefix/rust/tests/longest_common_prefix_test.rs
@@ -3,9 +3,9 @@ use leetcode::Solution;
 #[test]
 fn have_longest_common_prefix() {
 	let input: Vec<String> = vec![ "flower", "flow", "flight" ]
-							.into_iter()
-							.map(String::from)
-							.collect();
+		.into_iter()
+		.map(String::from)
+		.collect();
 	let expected: &str = "fl";
 
 	process_test_case(&input, expected);
@@ -14,9 +14,9 @@ fn have_longest_common_prefix() {
 #[test]
 fn no_longest_common_prefix() {
 	let input: Vec<String> = vec![ "dog", "racecar", "car" ]
-							.into_iter()
-							.map(String::from)
-							.collect();
+		.into_iter()
+		.map(String::from)
+		.collect();
 	let expected: &str = "";
 
 	process_test_case(&input, expected);
