49e1f34b6c67583f7ea9134597d3dabda183d8f7
rabbit19981023 <rabbit19981023@gmail.com>
2022-03-04 19:05:05 +0800

    refactoring 14.longest-common-prefix lang:cpp

--- 14.longest-common-prefix/cpp/src/longest_common_prefix.h
+++ 14.longest-common-prefix/cpp/src/longest_common_prefix.h
@@ -4,67 +4,87 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if (! isValid(strs)) {
-            return "";
-        }
-        
-        int low = 1;
-        int high = getMinLen(strs);
-        int mid;
+  string longestCommonPrefix(vector<string> &strs);
+  bool isValid(vector<string> &strs);
+  int getMinLen(vector<string> &strs);
+  bool isCommonPrefix(int mid, vector<string> &strs);
+  bool startsWith(string str, string match, int pos);
+};
 
-        while (low <= high) {
-            mid = (low + high) / 2;
+string Solution::longestCommonPrefix(vector<string> &strs)
+{
+  if (!isValid(strs))
+  {
+    return "";
+  }
 
-            if (isCommonPrefix(mid, strs)) {
-                low = mid + 1;
-                continue;
-            }
+  int low = 1;
+  int high = getMinLen(strs);
+  int mid;
 
-            high = mid - 1;
-        }
+  while (low <= high)
+  {
+    mid = (low + high) / 2;
 
-        int result = (low + high) / 2;
-        return strs[0].substr(0, result);
-    };
+    if (isCommonPrefix(mid, strs))
+    {
+      low = mid + 1;
+      continue;
+    }
 
-    bool isValid(vector<string>& strs) {
-        return ! ((typeid(strs) != typeid(vector<string>)) || (strs.size() == 0));
-    };
+    high = mid - 1;
+  }
 
-    int getMinLen(vector<string>& strs) {
-        int minLen = strs[0].length();
+  int result = (low + high) / 2;
+  return strs[0].substr(0, result);
+};
 
-        for (int i = 1; i < strs.size(); i++) {
-            if (strs[i].length() < minLen) {
-                minLen = strs[i].length();
-            }
-        }
+bool Solution::isValid(vector<string> &strs)
+{
+  return !((typeid(strs) != typeid(vector<string>)) || (strs.size() == 0));
+};
 
-        return minLen;
-    };
+int Solution::getMinLen(vector<string> &strs)
+{
+  int minLen = strs[0].length();
 
-    bool isCommonPrefix(int mid, vector<string>& strs) {
-        string prefix = strs[0].substr(0, mid);
+  for (int i = 1; i < strs.size(); i++)
+  {
+    if (strs[i].length() < minLen)
+    {
+      minLen = strs[i].length();
+    }
+  }
 
-        for (int i = 1; i < strs.size(); i++) {
-            if (! startsWith(strs[i], prefix, 0)) {
-                return false;
-            }
-        }
+  return minLen;
+};
 
-        return true;
-    };
+bool Solution::isCommonPrefix(int mid, vector<string> &strs)
+{
+  string prefix = strs[0].substr(0, mid);
 
-    bool startsWith(string str, string match, int pos) {
-        if (str.find(match) == pos) {
-            return true;
-        }
+  for (int i = 1; i < strs.size(); i++)
+  {
+    if (!startsWith(strs[i], prefix, 0))
+    {
+      return false;
+    }
+  }
 
-        return false;
-    };
+  return true;
+};
+
+bool Solution::startsWith(string str, string match, int pos)
+{
+  if (str.find(match) == pos)
+  {
+    return true;
+  }
+
+  return false;
 };
 
 /** Don't define main function, it may make GoogleTest can't find the Tests **/
